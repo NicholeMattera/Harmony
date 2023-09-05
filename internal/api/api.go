c46ef14828d86003c515870d85edbd849ec308c0
Nichole Mattera <me@nicholemattera.gay>
2023-09-04 21:02:45 -0500

    ✨ Made frontend and admin hosting optional

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,6 +1,10 @@
 package api
 
 import (
+	"net/http"
+	"os"
+	"strings"
+
 	"github.com/NicholeMattera/Harmony/internal/api/middleware"
 	"github.com/gin-gonic/gin"
 )
@@ -19,14 +23,41 @@ func SetupRouter() *gin.Engine {
 	router.Use(middleware.Database)
 	router.Use(middleware.Authorization)
 
-	router.StaticFile("/", "./assets/site.html")
-	router.StaticFile("/admin", "./assets/admin.html")
-	router.Static("/content", "./assets/content/")
-	router.StaticFile("/favicon.ico", "./assets/favicon.ico")
-	router.StaticFile("/robots.txt", "./assets/robots.txt")
+	// Frontend Routes
+	hostSite, hostSiteExists := os.LookupEnv("HARMONY_HOST_SITE")
+	hostAdmin, hostAdminExists := os.LookupEnv("HARMONY_HOST_ADMIN")
+	shouldHostSite := hostSiteExists && strings.ToLower(hostSite) == "true"
+	shouldHostAdmin := hostAdminExists && strings.ToLower(hostAdmin) == "true"
 
+	if shouldHostSite {
+		router.StaticFile("/index.html", "./assets/index.html")
+		router.StaticFile("/favicon.ico", "./assets/favicon.ico")
+		router.StaticFile("/robots.txt", "./assets/robots.txt")
+	}
+
+	if shouldHostAdmin {
+		router.StaticFile("/admin", "./assets/admin.html")
+	}
+
+	if shouldHostSite || shouldHostAdmin {
+		router.Static("/content", "./assets/content")
+		router.NoRoute(func(c *gin.Context) {
+			if shouldHostAdmin && len(c.Request.URL.Path) >= 5 && c.Request.URL.Path[:5] == "admin" {
+				c.Header("Content-Type", "text/html; charset=utf-8")
+				c.File("./dist/admin.html")
+			} else if shouldHostSite {
+				c.Header("Content-Type", "text/html; charset=utf-8")
+				c.File("./dist/index.html")
+			}
+
+			c.Status(http.StatusNotFound)
+		})
+	}
+
+	// Login Routes
 	NewLoginHandler(router)
 
+	// API Routes
 	apiV1RouterGroup := router.Group("/api/v1")
 	NewEntityHandler(apiV1RouterGroup)
 	NewRoleHandler(apiV1RouterGroup)
